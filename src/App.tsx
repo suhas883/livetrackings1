b3aed7fa55b669ad608eafb46aee09de96920b14
suhas883 <suhas883@gmail.com>
2025-11-24 16:26:48 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="container">
+      <h1>LiveTrackings</h1>
+      <p>Welcome to LiveTrackings - Your independent tracking solution</p>
+      <p>This site is deployed on Cloudflare Pages without Supabase!</p>
+    </div>
+  )
+}
